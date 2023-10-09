261550edd1c730a433d3150c5e7ce4115b0642ec
HamBrick327 <92757936+HamBrick327@users.noreply.github.com>
2023-10-08 22:47:27 -0600

    inital commit

---
+++ alternate.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+tinker = tk.Tk()
+
+## tkinter window stuff
+tinker.geometry("500x500")
+
+text = tk.Text(tinker, height=10, width=20)
+
+l = tk.Label(tinker, text = "input text")
+#l.config(font=("Courier", 14))
+
+b1 = tk.Button(tinker, text="button1", command= print("button1 pressed"))
+b2 = tk.Button(tinker, text="exit", command= tinker.destroy)
+
+l.pack()
+text.pack()
+b1.pack()
+b2.pack()
+
+# text.insert(tk.END, {text})
+
+tk.mainloop()
