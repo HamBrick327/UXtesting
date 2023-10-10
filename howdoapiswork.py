d4e43a1f088cdcfbae0ee09cb7960171a07eabf3
HamBrick327 <92757936+HamBrick327@users.noreply.github.com>
2023-10-09 23:01:22 -0600

    started testing customtkinter to make better looking ui

--- howdoapiswork.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-
-response = requests.get(f'https://api.api-ninjas.com/v1/jokes?limit={1}', headers={'X-Api-Key' : "FWIYfCodkizNPtWSfjMNYw==BpZw8KgtJ4foUCyJ"})
-
-if response.status_code == requests.codes.ok:
-    response = json.loads(response.text)
-    print(response[0]['joke'])
