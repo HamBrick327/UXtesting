778d0f03682a0b2c8ea08554f9c2fa76b90e746f
HamBrick327 <derpykidgamer@gmail.com>
2023-11-14 11:17:26 -0700

    updated the references comment to keep track of where I'm getting my code and such

--- alternate.py
+++ alternate.py
@@ -8,6 +8,7 @@ from time import time, sleep
 https://youtu.be/JDU-ycsxvqM?si=fYYgmRc_azqzHyww
 https://youtu.be/1itG8q-sCGY?si=r4HOdwhsYHHWl1U3
 https://github.com/TomSchimansky/CustomTkinter
+https://stackoverflow.com/questions/27215326/tkinter-keypress-and-keyrelease-events
 '''
 
 ctk.set_appearance_mode("Dark")  # Modes: system (default), light, dark
@@ -15,6 +16,7 @@ ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 app = ctk.CTk()  # create CTk window like you do with the Tk window
 app.geometry("500x500")
+# app.overrideredirect(True) ## this will remove the toolbar (x button, fullscreen button, minimize button)
 
 frame = ctk.CTkFrame(master=app)
 frame.pack()
